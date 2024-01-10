1bd897bd80cea2fe1887d526d264930f0756ffa5
Plamena Marinova <plamena.marinova@gmail.com>
2024-01-10 13:26:19 +0200

    Add leaflet map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { MapContainer, TileLayer } from 'react-leaflet';
+
 function App() {
   return (
-    <div id="container">
-      <h1>App Title</h1>
+    <div id="map">
+      <MapContainer center={[42.7339, 25.4858]} zoom={7} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
