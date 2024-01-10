aa2277e827601fdf83773b36e389bb0246e40b81
Plamena Marinova <plamena.marinova@gmail.com>
2024-01-10 13:31:39 +0200

    Change default map center point and zoom

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 function App() {
   return (
     <div id="map">
-      <MapContainer center={[42.7339, 25.4858]} zoom={7} scrollWheelZoom={true}>
+      <MapContainer center={[43.2073873, 27.9166653]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
