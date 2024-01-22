8cd4d876e473df207b51627141531c717feb1956
Plamena Marinova <plamena.marinova@gmail.com>
2024-01-22 14:01:46 +0200

    Fix load data on startup

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,36 @@ import { BuildingPermit, PropertyCoordinates } from './Data';
 import { BuildingPermitsMap, BuildingPermitsMapData } from './Map';
 import './App.css';
 
+const loadBuildingPermits = async () => {
+  const res = await fetch(process.env.PUBLIC_URL + '/data/building_permits.json');
+  const permits = await res.json() as BuildingPermit[];
+  return permits;
+};
+
+const loadPropertyCoordinates = async () => {
+  const res = await fetch(process.env.PUBLIC_URL + '/data/geo_coordinates.json');
+  const coordinates = await res.json() as PropertyCoordinates;
+  return coordinates;
+};
+
 function App() {
 
   const [data, setData] = useState({ permits: [], coordinates: {}} as BuildingPermitsMapData);
   
   useEffect(() => {
+    
+    const loadData = async () => {
+      const buildingPermits = await loadBuildingPermits();
+      const propertyCoordinates = await loadPropertyCoordinates();
+      setData({
+        permits: buildingPermits.filter(permit => propertyCoordinates[permit.propertyId]),
+        coordinates: propertyCoordinates
+      });
+    };
+
     loadData();
-  });
-  
-  const loadData = async () => {
-    const buildingPermits = await loadBuildingPermits();
-    const propertyCoordinates = await loadPropertyCoordinates();
-    setData({
-      permits: buildingPermits.filter(permit => propertyCoordinates[permit.propertyId]),
-      coordinates: propertyCoordinates
-    });
-  };
 
-  const loadBuildingPermits = async () => {
-    const res = await fetch(process.env.PUBLIC_URL + '/data/building_permits.json');
-    const permits = await res.json() as BuildingPermit[];
-    return permits;
-  };
-
-  const loadPropertyCoordinates = async () => {
-    const res = await fetch(process.env.PUBLIC_URL + '/data/geo_coordinates.json');
-    const coordinates = await res.json() as PropertyCoordinates;
-    return coordinates;
-  };
+  }, []);
   
   return <BuildingPermitsMap data={data} />;
 }
