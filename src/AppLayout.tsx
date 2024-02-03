f5ded4206ae470309d8b575c677cb31b885e1ce0
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-03 14:27:32 +0200

    Add floating filter button and bottom drawer

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Drawer from '@mui/material/Drawer';
+import Fab from '@mui/material/Fab';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import MenuIcon from '@mui/icons-material/Menu';
 
-import { useMediaQuery, useTheme } from '@mui/material';
+import { styled, useMediaQuery, useTheme } from '@mui/material';
 
 
 // see https://github.com/mui/material-ui/issues/10739
@@ -32,13 +35,32 @@ const useAppBarHeight = () => {
 };
 
 
+const FloatingButton = styled(Fab)(({ theme }) => ({
+  position: 'fixed',
+  bottom: theme.spacing(4),
+  right: theme.spacing(2)
+}));
+
+
 type AppLayoutProps = {
   children: React.ReactNode;
 };
 
 const AppLayout = ({ children }: AppLayoutProps) => {
-  
+
   const appBarHeight = useAppBarHeight();
+  
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <Container maxWidth={false} disableGutters>
@@ -60,6 +82,14 @@ const AppLayout = ({ children }: AppLayoutProps) => {
       <Box component="main" sx={{ flexGrow: 1, height: `calc(100vh - ${appBarHeight}px)` }}>
         {children}
       </Box>
+      <FloatingButton color="primary" aria-label="filter" onClick={toggleDrawer}>
+        <FilterAltIcon />
+      </FloatingButton>
+      <Drawer anchor="bottom" open={drawerOpen} onClose={toggleDrawer}>
+        <Box sx={{ height: '40vh' }}>
+          
+        </Box>
+      </Drawer>
     </Container>
   );
 };
