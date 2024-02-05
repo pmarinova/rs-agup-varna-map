eee539395f7997bf5353a58df41f3a58437b8c25
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-05 10:29:35 +0200

    Switch to BG localization

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { bgBG as localizationCoreComponents } from '@mui/material/locale';
+import { bgBG as localizationDataGrid } from '@mui/x-data-grid';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -11,12 +15,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App';
 
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: '#1976d2' },
+    },
+  },
+  localizationCoreComponents,
+  localizationDataGrid
+);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
