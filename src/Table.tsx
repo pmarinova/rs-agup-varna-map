9484d584448cd8b90ecf384c77d6d0c313803f1c
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-07 11:45:31 +0200

    Add proper table sorting by permit id

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import { DataGrid } from '@mui/x-data-grid';
-import { GridColDef } from '@mui/x-data-grid';
+import { GridColDef, GridComparatorFn } from '@mui/x-data-grid';
 
 import { BuildingPermit } from './Data';
+
+const permitIdComparator: GridComparatorFn<string> = (id1, id2) => {
+  const [number1, date1] = id1.split('/');
+  const [number2, date2] = id2.split('/');
+  return date1.localeCompare(date2) || (parseInt(number1) - parseInt(number2));
+};
 
 type BuildingPermitsTableData = {
   permits: BuildingPermit[];
@@ -16,8 +22,17 @@ type BuildingPermitsTableProps = {
 const BuildingPermitsTable = ({ data }: BuildingPermitsTableProps) => {
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'Разрешение №', flex: 10 },
-    { field: 'description', headerName: 'Обект', flex: 90 },
+    {
+      field: 'id', 
+      headerName: 'Разрешение №', 
+      flex: 10,
+      sortComparator: permitIdComparator
+    },
+    {
+      field: 'description', 
+      headerName: 'Обект', 
+      flex: 90
+    },
   ];
 
   return (
